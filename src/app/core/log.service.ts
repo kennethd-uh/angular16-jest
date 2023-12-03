c5f428b679f6cb174b19f96ed0257511ebf35cd3
Kenneth Dombrowski <kdombrowski@unitehere.org>
2023-12-03 18:26:28 -0500

    simple LogService implementation

--- src/app/core/log.service.ts
+++ src/app/core/log.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
 
+export abstract class BaseLogHandler {
+  // TODO logLevel: LogLevel = LOG_LEVEL;
+  abstract log(msg: any): void;
+}
+
+export class ConsoleLogHandler extends BaseLogHandler {
+  log(msg: any): void { console.log(msg); }
+}
+
+export class AppInsightsLogHandler extends BaseLogHandler {
+  log(msg: any): void {
+    // TODO AppInsightsLogService.log(msg);
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
+  handlers: BaseLogHandler[] = [];
 
-  constructor() { }
+  constructor() {
+    this.handlers.push(new ConsoleLogHandler());
+  }
+
+  addHandler(logHandler: BaseLogHandler) {
+    this.handlers.push(logHandler);
+  }
+
+  log(msg: any) {
+    var datestr: string = new Date().toISOString();
+    var msgstr: string = JSON.stringify(msg);
+
+    for (var i = 0; i < this.handlers.length; i++) {
+      var handler: BaseLogHandler = this.handlers[i];
+      console.log(handler);
+      handler.log(datestr + ": " + msgstr);
+    }
+  }
+
+  error(msg: any) { this.log('ERROR: ' + msg); }
+  warning(msg: any) { this.log('WARNING: ' + msg); }
+  info(msg: any) { this.log('INFO: ' + msg); }
+  debug(msg: any) { this.log('DEBUG: ' + msg); }
 }
