0c1187d7d28f85a98d9fad26b6d4172716352319
Kenneth Dombrowski <kdombrowski@unitehere.org>
2023-12-03 13:23:29 -0500

    first custom test(): 'can mock axios'

--- src/app/core/api.service.spec.ts
+++ src/app/core/api.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
 
+import axios from 'axios';
+jest.mock("axios");
+
 describe('ApiService', () => {
   let service: ApiService;
 
@@ -13,4 +16,12 @@ describe('ApiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  test('can mock axios', async () => {
+    const mockResp = {data: {body: 'foo'}}
+    axios.get = jest.fn().mockResolvedValue(mockResp)
+    const axiosResp = await axios.get('http://this.is.not.a.real.url')
+    expect(axiosResp).toEqual(mockResp)
+  });
+
 });
