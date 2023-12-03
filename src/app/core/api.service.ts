d6f6be67da10b96e1443294d9c65fb2a2cf8c520
Kenneth Dombrowski <kdombrowski@unitehere.org>
2023-12-03 13:22:59 -0500

    add ApiService.get() & ApiService.post() methods

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,14 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor() { }
+
+  async get(url: string, config?: any) {
+    const response = await axios.get(url, config).catch(error => error.response)
+    return response
+  }
+
+  async post(url: any, data: any, config?: any) {
+    const response = await axios.post(url, data, config).catch(error => error.reponse)
+    return response
+  }
 }
