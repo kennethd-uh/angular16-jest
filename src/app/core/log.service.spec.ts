700cd8cba4bbc3da8fd3f7448dc55c5ed48d903c
Kenneth Dombrowski <kdombrowski@unitehere.org>
2024-02-01 15:51:34 -0500

    LogService

--- src/app/core/log.service.spec.ts
+++ src/app/core/log.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LogService } from './log.service';
+import { LogService, ConsoleLogHandler, AppInsightsLogHandler, Severity } from './log.service';
 
 describe('LogService', () => {
   let service: LogService;
+  let mockConsoleLog: any;
+  let mockAppInsightsLog: any;
+  let mockLogServiceNow: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ ],
+      providers: [ ]
+    });
     service = TestBed.inject(LogService);
+
+    mockConsoleLog = jest.spyOn(ConsoleLogHandler.prototype, 'log')
+      .mockImplementation(async(datestr: string, severity: Severity, msg: any) => {});
+
+    mockAppInsightsLog = jest.spyOn(AppInsightsLogHandler.prototype, 'log')
+      .mockImplementation(async(datestr: string, severity: Severity, msg: any) => {});
+
+    mockLogServiceNow = jest.spyOn(service, 'now').mockReturnValue('the time is now');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  test('LogService automatically adds ConsoleLog to handlers & passes Severity appropriately', async () => {
+    service.debug("debug message");
+    service.info("info message");
+    service.warning("warning message");
+    service.error("error message");
+    expect(mockConsoleLog.mock.calls).toEqual([
+      ['the time is now', Severity.DEBUG, 'debug message'],
+      ['the time is now', Severity.INFO, 'info message'],
+      ['the time is now', Severity.WARNING, 'warning message'],
+      ['the time is now', Severity.ERROR, 'error message'],
+    ]);
+  });
+
+  test('LogService.log() calls log() with expected args for all registered handlers', async () => {
+    service.addHandler(new AppInsightsLogHandler());
+    service.debug("debug message");
+    expect(mockConsoleLog.mock.calls).toEqual([
+      ['the time is now', Severity.DEBUG, 'debug message'],
+    ]);
+    expect(mockAppInsightsLog.mock.calls).toEqual([
+      ['the time is now', Severity.DEBUG, 'debug message'],
+    ]);
+  });
+
 });
--- src/app/core/log.service.ts
+++ src/app/core/log.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 
+// numeric severity values allow for mathematical threshold implementation for
+// handlers; ts enums also provide reverse mapping so Severity[10] == "DEBUG"
+// https://www.typescriptlang.org/docs/handbook/enums.html#reverse-mappings
+export enum Severity {
+    DEBUG = 10,
+    INFO = 20,
+    WARNING = 30,
+    ERROR = 40,
+}
+
 export abstract class BaseLogHandler {
-  // TODO logLevel: LogLevel = LOG_LEVEL;
-  abstract log(msg: any): void;
+  public abstract log(datestr: string, severity: Severity, msg: any): Promise<void>;
 }
 
 export class ConsoleLogHandler extends BaseLogHandler {
-  log(msg: any): void { console.log(msg); }
+  public async log(datestr: string, severity: Severity, msg: any): Promise<void> {
+    // reverse-map Severity value back to human-readable name
+    console.log(datestr, Severity[severity], msg);
+  }
 }
 
 export class AppInsightsLogHandler extends BaseLogHandler {
-  log(msg: any): void {
+  public async log(datestr: string, severity: Severity, msg: any): Promise<void> {
     // TODO AppInsightsLogService.log(msg);
   }
 }
@@ -29,19 +41,22 @@ export class LogService {
     this.handlers.push(logHandler);
   }
 
-  log(msg: any) {
-    var datestr: string = new Date().toISOString();
-    var msgstr: string = JSON.stringify(msg);
+  now() {
+    return new Date().toISOString();
+  }
+
+  async log(severity: Severity, msg: any) {
+    // datestr should be exactly the same for all handlers
+    var datestr: string = this.now();
 
     for (var i = 0; i < this.handlers.length; i++) {
       var handler: BaseLogHandler = this.handlers[i];
-      console.log(handler);
-      handler.log(datestr + ": " + msgstr);
+      handler.log(datestr, severity, msg);
     }
   }
 
-  error(msg: any) { this.log('ERROR: ' + msg); }
-  warning(msg: any) { this.log('WARNING: ' + msg); }
-  info(msg: any) { this.log('INFO: ' + msg); }
-  debug(msg: any) { this.log('DEBUG: ' + msg); }
+  async error(msg: any) { this.log(Severity.ERROR, msg); }
+  async warning(msg: any) { this.log(Severity.WARNING, msg); }
+  async info(msg: any) { this.log(Severity.INFO, msg); }
+  async debug(msg: any) { this.log(Severity.DEBUG, msg); }
 }
